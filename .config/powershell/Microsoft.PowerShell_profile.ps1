c6bd1be8410559e54ab18632a4f25041738cf86f
off-origami <122206833+off-origami@users.noreply.github.com>
2024-03-24 05:48:33 -0700

    Create Microsoft.PowerShell_profile.ps1

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+# Since I have it on OneDrive (yes, I know, funny), I write it here: C:\Users\baska\OneDrive\Documents\PowerShell otherwise it won't see the file
+. $env:USERPROFILE\.config\powershell\user_profile.ps1
