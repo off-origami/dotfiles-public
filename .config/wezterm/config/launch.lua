2304b979ddba59a9647f7a3e84f330d5b02e7931
Igor Baskakov <122206833+off-origami@users.noreply.github.com>
2024-03-30 09:08:01 -0700

    Add files via upload

---
+++ .config/wezterm/config/launch.lua
@@ -0,0 +1,34 @@
+local platform = require('utils.platform')()
+
+local options = {
+   default_prog = {},
+   launch_menu = {},
+}
+
+if platform.is_win then
+   options.default_prog = { 'pwsh' }
+   options.launch_menu = {
+      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Desktop', args = { 'powershell' } },
+      { label = 'Command Prompt', args = { 'cmd' } },
+      { label = 'Nushell', args = { 'nu' } },
+      {
+         label = 'Git Bash',
+         args = { '%USERPROFILE%\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+      },
+      {
+         label = 'Alma Linux',
+         args = { 'ssh', 'kali@', '-p', '22' },
+      },
+   }
+elseif platform.is_mac then
+   options.default_prog = { '/opt/homebrew/bin/fish' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash' } },
+      { label = 'Fish', args = { '/opt/homebrew/bin/fish' } },
+      { label = 'Nushell', args = { '/opt/homebrew/bin/nu' } },
+      { label = 'Zsh', args = { 'zsh' } },
+   }
+end
+
+return options
