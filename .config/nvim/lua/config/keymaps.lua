bc9670d1b9829308033c3c39886fa9e24dc87edf
off-origami <122206833+off-origami@users.noreply.github.com>
2024-08-19 18:15:42 +0200

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,77 +2,95 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local discipline = require("craftzdog.discipline")
-
-discipline.cowboy()
-
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+
+-- Resume telescope
+keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume last Telescope picker" }
+)
 
 -- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
-keymap.set("n", "<Leader>dd", '"_dd')
-keymap.set("n", "<Leader>DD", '"_DD')
-
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+keymap.set("n", "x", '"_x', { noremap = true, silent = true, desc = "Delete without affecting register" })
+keymap.set("n", "<Leader>p", '"0p', { noremap = true, silent = true, desc = "Paste from yank register" })
+keymap.set("n", "<Leader>P", '"0P', { noremap = true, silent = true, desc = "Paste before from yank register" })
+keymap.set("v", "<Leader>p", '"0p', { noremap = true, silent = true, desc = "Paste over selection from yank register" })
+keymap.set("n", "<Leader>c", '"_c', { noremap = true, silent = true, desc = "Change without affecting register" })
+keymap.set(
+  "n",
+  "<Leader>C",
+  '"_C',
+  { noremap = true, silent = true, desc = "Change to end of line without affecting register" }
+)
+keymap.set(
+  "v",
+  "<Leader>c",
+  '"_c',
+  { noremap = true, silent = true, desc = "Change selection without affecting register" }
+)
+keymap.set(
+  "v",
+  "<Leader>C",
+  '"_C',
+  { noremap = true, silent = true, desc = "Change selection to end of line without affecting register" }
+)
+keymap.set("n", "<Leader>d", '"_d', { noremap = true, silent = true, desc = "Delete without affecting register" })
+keymap.set(
+  "n",
+  "<Leader>D",
+  '"_D',
+  { noremap = true, silent = true, desc = "Delete to end of line without affecting register" }
+)
+keymap.set(
+  "v",
+  "<Leader>d",
+  '"_d',
+  { noremap = true, silent = true, desc = "Delete selection without affecting register" }
+)
+keymap.set(
+  "v",
+  "<Leader>D",
+  '"_D',
+  { noremap = true, silent = true, desc = "Delete selection to end of line without affecting register" }
+)
+keymap.set(
+  "n",
+  "<Leader>dd",
+  '"_dd',
+  { noremap = true, silent = true, desc = "Delete line without affecting register" }
+)
+keymap.set(
+  "n",
+  "<Leader>DD",
+  '"_DD',
+  { noremap = true, silent = true, desc = "Delete entire line without affecting register" }
+)
 
 -- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
+keymap.set("n", "dw", 'vb"_d', { noremap = true, silent = true, desc = "Delete word backwards" })
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
-
--- Save with root permission (not working for now)
---vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
-
--- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
-
--- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+keymap.set("n", "<C-a>", "gg<S-v>G", { noremap = true, silent = true, desc = "Select all text" })
 
 -- New tab
-keymap.set("n", "te", ":tabedit")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "te", ":tabedit<CR>", { noremap = true, silent = true, desc = "Open new tab" })
+keymap.set("n", "<tab>", ":tabnext<CR>", { noremap = true, silent = true, desc = "Go to next tab" })
+keymap.set("n", "<s-tab>", ":tabprev<CR>", { noremap = true, silent = true, desc = "Go to previous tab" })
+
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
--- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+keymap.set("n", "sh", "<C-w>s", { noremap = true, silent = true, desc = "Split window horizontally" })
+keymap.set("n", "sv", ":vsplit<CR>", { noremap = true, silent = true, desc = "Split window vertically" })
+
+-- Move focus between windows
+keymap.set("n", "sh", "<C-w>h", { noremap = true, silent = true, desc = "Move focus to left window" })
+keymap.set("n", "sk", "<C-w>k", { noremap = true, silent = true, desc = "Move focus to upper window" })
+keymap.set("n", "sj", "<C-w>j", { noremap = true, silent = true, desc = "Move focus to lower window" })
+keymap.set("n", "sl", "<C-w>l", { noremap = true, silent = true, desc = "Move focus to right window" })
 
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
-
--- Diagnostics
-keymap.set("n", "<C-j>", function()
-	vim.diagnostic.goto_next()
-end, opts)
-
-keymap.set("n", "<leader>r", function()
-	require("craftzdog.hsl").replaceHexWithHSL()
-end)
-
-keymap.set("n", "<leader>i", function()
-	require("craftzdog.lsp").toggleInlayHints()
-end)
+keymap.set("n", "<C-w><left>", "<C-w><", { noremap = true, silent = true, desc = "Decrease window width" })
+keymap.set("n", "<C-w><right>", "<C-w>>", { noremap = true, silent = true, desc = "Increase window width" })
+keymap.set("n", "<C-w><up>", "<C-w>+", { noremap = true, silent = true, desc = "Increase window height" })
+keymap.set("n", "<C-w><down>", "<C-w>-", { noremap = true, silent = true, desc = "Decrease window height" })
