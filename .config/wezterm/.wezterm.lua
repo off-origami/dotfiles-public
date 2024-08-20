a71037905fa1c490e5e685627b731d32a9f17345
off-origami <122206833+off-origami@users.noreply.github.com>
2024-08-20 18:30:09 +0200

    Update and rename wezterm.lua to .wezterm.lua

---
+++ .config/wezterm/.wezterm.lua
@@ -0,0 +1,69 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = true,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	default_cursor_style = "BlinkingBar",
+	colors = {
+		foreground = "#CBE0F0",
+		background = "#011423",
+		cursor_bg = "#47FF9C",
+		cursor_border = "#47FF9C",
+		cursor_fg = "#011423",
+		selection_bg = "#033259",
+		selection_fg = "#CBE0F0",
+		ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+		brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	},
+	font = wezterm.font("JetBrains Mono Regular"),
+	font_size = 13.5,
+	background = {
+		{
+			source = {
+				File = "wallpaper/wallpaperflare.com_wallpaper.jpg",
+			},
+			hsb = {
+				hue = 1.0,
+				saturation = 1.02,
+				brightness = 0.25,
+			},
+			-- width = "100%",
+			--height = "100%",
+			vertical_align = "Middle",
+			horizontal_align = "Center",
+		},
+		{
+			source = {
+				Color = "#282c35",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.75,
+		},
+	},
+	window_padding = {
+		left = 25,
+		right = 20,
+		top = 25,
+		bottom = 0,
+	},
+
+	enable_wayland = false,
+	front_end = "OpenGL",
+}
+
+config.keys = {
+	{
+		key = "f",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+return config
